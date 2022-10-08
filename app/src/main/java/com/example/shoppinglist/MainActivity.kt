a15e9cff64c5081d68f4bba52519a8988886ac05
mahmoudibrahimmohammad <mahmoud.ibrahim.mohammad@rikstv.no>
2022-10-08 14:47:12 +0200

    Show MainScreen in MainActivity.kt

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.shoppinglist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,11 +11,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.shoppinglist.component.MainScreen
 import com.example.shoppinglist.ui.theme.ShoppingListTheme
+import com.example.shoppinglist.viewmodels.ShoppingListViewModel
+import com.example.shoppinglist.viewmodels.UserViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val userViewModel : UserViewModel by viewModels()
+        val shoppingListViewModel : ShoppingListViewModel by viewModels()
         setContent {
             ShoppingListTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,22 +30,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainScreen(userViewModel = userViewModel, shoppingListViewModel = shoppingListViewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ShoppingListTheme {
-        Greeting("Android")
     }
 }
