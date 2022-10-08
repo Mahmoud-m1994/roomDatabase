43e6a98fe06c0472b6a1fdff343ae72fd9140a1e
mahmoudibrahimmohammad <mahmoud.ibrahim.mohammad@rikstv.no>
2022-10-08 14:33:24 +0200

    Added ShoppingListApp to AndroidManifest.xml to use AppModule when creating viewModels

---
+++ app/src/main/java/com/example/shoppinglist/ShoppingListApp.kt
@@ -0,0 +1,11 @@
+package com.example.shoppinglist
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ShoppingListApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
