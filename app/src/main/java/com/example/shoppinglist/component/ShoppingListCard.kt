ea751eb5b95ae4f5f877d5d1b49b5d46d05c334b
mahmoudibrahimmohammad <mahmoud.ibrahim.mohammad@rikstv.no>
2022-10-14 10:08:15 +0200

    Refactoring ShoppingListCard

--- app/src/main/java/com/example/shoppinglist/component/ShoppingListCard.kt
+++ app/src/main/java/com/example/shoppinglist/component/ShoppingListCard.kt
@@ -1,50 +1,61 @@
 package com.example.shoppinglist.component
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.shoppinglist.R
 import com.example.shoppinglist.models.ShoppingList
 
 
 @Composable
-fun ShoppingListCard(shoppingList: ShoppingList) {
+fun ShoppingListCard(
+    shoppingList: ShoppingList,
+    onDeleteClicked: (ShoppingList) -> Unit,
+    onEditClicked: (ShoppingList) -> Unit,
+    onCardClicked: (ShoppingList) -> Unit
+) {
     Card(
         modifier = Modifier
-            .padding(16.dp)
-            .shadow(
-                4.dp,
-                spotColor = Color.Black,
-                ambientColor = Color.Gray
-            )
-            .background(Color.White),
-        elevation = 4.dp
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable(onClick = { onCardClicked(shoppingList) }),
+        elevation = 10.dp
     ) {
         Column(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            modifier = Modifier.padding(15.dp)
         ) {
-            Text(
-                text = shoppingList.shoppingList_name,
-                fontSize = 40.sp,
-                fontFamily = FontFamily.SansSerif,
-                fontStyle = FontStyle.Italic
-            )
+            Text(shoppingList.shoppingList_name)
+        }
+        Row(horizontalArrangement = Arrangement.End) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                modifier = Modifier.padding(16.dp)
+            ) {
+                CardIcon(
+                    description = stringResource(R.string.edit_shopping_list),
+                    imageVector = Icons.Rounded.Edit,
+                    onClick = { onEditClicked(shoppingList) }
+                )
+                CardIcon(
+                    description = stringResource(R.string.delete_shopping_List),
+                    imageVector = Icons.Rounded.Delete,
+                    imageVectorColor = Color.Red,
+                    onClick = { onDeleteClicked(shoppingList) }
+                )
+            }
         }
     }
 }
